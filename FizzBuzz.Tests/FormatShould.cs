26b338f2cfadaa54a64ae822cc629e2792c1691d
Wes Vollmar <wcvollmar@marathonpetroleum.com>
2017-09-21 09:27:50 -0400

    Completed solution

---
+++ FizzBuzz.Tests/FormatShould.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass]
+    public class FormatShould
+    {
+        [TestMethod]
+        public void ReturnFizzGiven3()
+        {
+            var result = FizzBuzzLogic.Format(3);
+
+            Assert.IsTrue(result == "Fizz");
+        }
+
+        [TestMethod]
+        public void ReturnBuzzGiven5()
+        {
+            var result = FizzBuzzLogic.Format(5);
+
+            Assert.IsTrue(result == "Buzz");
+        }
+
+        [TestMethod]
+        public void ReturnFizzBuzzGiven15()
+        {
+            var result = FizzBuzzLogic.Format(15);
+
+            Assert.IsTrue(result == "FizzBuzz");
+        }
+
+        [TestMethod]
+        public void Return4Given4()
+        {
+            var result = FizzBuzzLogic.Format(4);
+
+            Assert.IsTrue(result == "4");
+        }
+    }
+}
