ce54b066d0a3a587438b566be1ca80242e5a686d
Wes Vollmar <wcvollmar@marathonpetroleum.com>
2017-09-21 09:27:50 -0400

    Completed solution

--- FizzBuzz/FizzBuzzLogic.cs
+++ FizzBuzz/FizzBuzzLogic.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace FizzBuzz
 {
@@ -11,10 +7,38 @@ namespace FizzBuzz
     /// </summary>
     public static class FizzBuzzLogic
     {
+        /// <summary>
+        /// Method to run the FizzBuzz process for a given number of records
+        /// </summary>
+        /// <param name="numberOfResults">Number to count</param>
+        /// <returns>IEnumerable of the output results</returns>
         public static IEnumerable<string> FizzBuzz(int numberOfResults = 10)
         {
-            // TODO - Implement FizzBuzz here
-            throw new NotImplementedException();
+            var results = new List<string>();
+
+            for (int i = 1; i <= numberOfResults; i++)
+                results.Add(Format(i));
+
+            return results;
+        }
+
+        /// <summary>
+        /// Method to convert a given number to the desired output
+        /// </summary>
+        /// <param name="number">Number to format</param>
+        /// <returns>Formatted output result</returns>
+        public static string Format(int number)
+        {
+            var result = number.ToString();
+
+            if (number % 3 == 0 && number % 5 == 0)
+                result = "FizzBuzz";
+            else if (number % 3 == 0)
+                result = "Fizz";
+            else if (number % 5 == 0)
+                result = "Buzz";
+
+            return result;
         }
     }
 }
